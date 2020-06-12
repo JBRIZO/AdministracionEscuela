451dffeb87927cd8590de43bca14ccac67ea2da2
JBRIZO <jonathan.laux@hotmail.com>
2020-06-11 22:48:31 -0600

    Programa terminado

--- src/Proyectos/Escuela.java
+++ src/Proyectos/Escuela.java
@@ -2,7 +2,7 @@ package Proyectos;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.*;
 public class Escuela {
     private ArrayList<Estudiante> estudiantes = new ArrayList<>();
     private ArrayList<Maestro> maestros = new ArrayList<>();
@@ -41,7 +41,7 @@ public class Escuela {
         estudiante.setNombre(nombre);
         System.out.println("Ingrese el grado del estudiante:");
         estudiante.setGrado();
-        estudiante.setId(generarID(estudiante));
+        generarID(estudiante);
         System.out.println("Estudiante ingresado con ID: " + estudiante.getId());
         estudiantes.add(estudiante);
         corr++;
@@ -61,7 +61,7 @@ public class Escuela {
         System.out.print("L.");
         double salario = lt.obtenerDoubleValidado("Ingrese un numero");
         maestro.setSalario(salario);
-        maestro.setId(generarIDMaestro());
+        generarIDMaestro(maestro);
         System.out.println("Maestro ingresado con ID: "+ maestro.getId());
         maestros.add(maestro);
     }
@@ -95,61 +95,125 @@ public class Escuela {
         Scanner sc = new Scanner(System.in);
         LectorTeclado lt = LectorTeclado.getInstance();
         System.out.println("Porfavor ingrese su id:");
-        String id = sc.nextLine();
-        for (int i = 0; i < estudiantes.size(); i++)
+        boolean salir = false;
+        while (!salir)
         {
-            if (id.equals(estudiantes.get(i).getId())) {
-                System.out.println("Nombre: " + estudiantes.get(i).getNombre());
-                System.out.println("Grado: " + estudiantes.get(i).getGrado());
-                System.out.println("Ingrese la cantidad que desea pagar:");
-                System.out.print("L.");
-                double pago = lt.obtenerDoubleValidado("Ingrese una cantidad valida");
-                ingresos.add(pago);
-                estudiantes.get(i).agregarPago(pago);
-                System.out.println(" -------------------------");
-                System.out.println(" Pago realizado con exito ");
-                System.out.println(" -------------------------");
-            }else{
-                System.out.println("ID Invalido");
+            int control = 0;
+            String id = sc.nextLine();
+            for (int i = 0; i < estudiantes.size(); i++)
+            {
+                if (id.equals(estudiantes.get(i).getId()))
+                {
+                    System.out.println("Nombre: " + estudiantes.get(i).getNombre());
+                    System.out.println("Grado: " + estudiantes.get(i).getGrado());
+                    System.out.println("Ingrese la cantidad que desea pagar:");
+                    System.out.print("L.");
+                    double pago = lt.obtenerDoubleValidado("Ingrese una cantidad valida");
+                    ingresos.add(pago);
+                    estudiantes.get(i).agregarPago(pago);
+                    System.out.println(" -------------------------");
+                    System.out.println(" Pago realizado con exito ");
+                    System.out.println(" -------------------------");
+                    salir = true;
+                    break;
+                }
+                control++;
+            }
+            if (control == estudiantes.size())
+            {
+                System.out.println("ID invalido");
             }
         }
+
     }
     public void mostrarPagosRealizados()
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Porfavor ingrese su id:");
-        String id = sc.nextLine();
-        double total = 0;
-        for (int i = 0; i < estudiantes.size(); i++)
+        boolean salir = false;
+        while (!salir)
         {
-            if (id.equals(estudiantes.get(i).getId()))
+            int control = 0;
+            String id = sc.nextLine();
+            double total = 0;
+            for (int i = 0; i < estudiantes.size(); i++)
             {
-                System.out.println("Nombre: " + estudiantes.get(i).getNombre());
-                System.out.println("Grado: " + estudiantes.get(i).getGrado());
-                System.out.println("Usted ha realizado los siguientes pagos: ");
-                for (int j = 0; j < estudiantes.get(i).getPagosSize(); j++ )
+                if (id.equals(estudiantes.get(i).getId()))
                 {
-                    System.out.print("L.");
-                    System.out.println(estudiantes.get(i).getPagosRealizados(j));
-                    total = total + estudiantes.get(i).getPagosRealizados(j);
+                    System.out.println("Nombre: " + estudiantes.get(i).getNombre());
+                    System.out.println("Grado: " + estudiantes.get(i).getGrado());
+                    System.out.println("Usted ha realizado los siguientes pagos: ");
+                    for (int j = 0; j < estudiantes.get(i).getPagosSize(); j++ )
+                    {
+                        System.out.print((j+1)+".");
+                        System.out.print("L.");
+                        System.out.println(estudiantes.get(i).getPagosRealizados(j));
+                        total = total + estudiantes.get(i).getPagosRealizados(j);
+                    }
+                    break;
                 }
+                control++;
             }
+            if (control == estudiantes.size())
+            {
+                System.out.println("ID invalido");
+            }else{
+                System.out.println("Con un total de: ");
+                System.out.print("L.");
+                System.out.println(total);
+                salir = true;
+            }
+
         }
-        System.out.println("Con un total de: ");
-        System.out.print("L.");
-        System.out.println(total);
+
     }
 
-    public String generarID(Estudiante estudiante)
+    public void generarID(Estudiante estudiante)
     {
-        String id = "2020-"+estudiante.getGrado() + corr;
-        return id;
+        Random random = new Random();
+        boolean unico = false;
+
+        while(!unico){
+            String id = "2020" + estudiante.getGrado();
+            for(int i=0; i<4; i++){
+                id = id + random.nextInt(10);
+            }
+
+            unico = true;
+
+            for(int i=0; i<estudiantes.size(); i++){
+                Estudiante estComp = estudiantes.get(i);
+                if(estComp.getId().equals(id)){
+                    unico = false;
+                    break;
+                }
+            }
+            estudiante.setId(id);
+        }
     }
-    public String generarIDMaestro()
+
+    public void generarIDMaestro(Maestro maestro)
     {
-        String id = "2020-"+corr2;
-        corr2++;
-        return id;
+        Random random = new Random();
+        boolean unico = false;
+
+        while(!unico){
+            String id = "2020";
+            for(int i=0; i<4; i++){
+                id = id + random.nextInt(10);
+            }
+
+            unico = true;
+
+            for(int i=0; i<maestros.size(); i++){
+                Maestro estComp = maestros.get(i);
+                if(estComp.getId().equals(id)){
+                    unico = false;
+                    break;
+                }
+            }
+            maestro.setId(id);
+        }
     }
 
 
